c74cbcb06cce6e83c1eef82cfda8ff00829de688
wato787 <wato787@icloud.com>
2023-05-06 16:44:38 +0900

    2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
+    <Provider store={store}>
+
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
--- slices/userSlice.ts
+++ slices/userSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "../types/types";
-// import type { RootState } from "../store";
+import type { RootState } from "../store";
 
 type State = {
   user: User;
@@ -24,6 +24,6 @@ export const userSlice = createSlice({
 });
 export const { login, logout } = userSlice.actions;
 
-// export const selectUser = (state: RootState) => state.user.user;
+export const selectUser = (state: RootState) => state.user.user;
 
 export default userSlice.reducer;
